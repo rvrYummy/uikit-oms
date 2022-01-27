708398c990d4f5738e89c33b0de884691542ddb4
rvrYummy <muchammad.rivari@yummycorp.com>
2022-01-27 21:00:53 +0700

    chore: release v1.0.1

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import './styles/index.css'
 import './styles/fonts.css'
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+export * from './components/Button';
+export * from './components/Card';
+export * from './components/Checkbox';
+export * from './components/Form';
+export * from './components/RadioButton';
+export * from './components/Text';
+export * from './components/Title';
